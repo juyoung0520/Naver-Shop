94f3e7bc6b87daa49281385f7cff31028855b1d6
Juyoung Jin <juyoung0520@naver.com>
2021-03-30 18:55:40 +0900

    implement Recyclerview in ItemFragment

--- app/src/main/java/com/example/navershop/ItemsFragment.kt
+++ app/src/main/java/com/example/navershop/ItemsFragment.kt
@@ -1,10 +1,17 @@
 package com.example.navershop
 
+import android.icu.lang.UCharacter
 import android.os.Bundle
+import android.service.autofill.Dataset
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_items.view.*
 
 class ItemsFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +19,30 @@ class ItemsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_items, container, false)
+        val view = inflater.inflate(R.layout.fragment_items, container, false)
+
+        view.recycler_item.adapter = itemRecyclerview()
+        view.recycler_item.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        return view
+    }
+
+    inner class itemRecyclerview() : RecyclerView.Adapter<itemRecyclerview.ViewHolder>() {
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): itemRecyclerview.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        }
+
+        override fun getItemCount(): Int {
+            return
+        }
     }
 }
